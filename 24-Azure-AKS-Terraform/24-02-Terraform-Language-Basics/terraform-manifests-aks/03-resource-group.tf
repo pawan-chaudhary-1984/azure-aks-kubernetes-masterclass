88dd26167697222da28118bd33bd7908022fbe36
pawan-chaudhary-1984 <pawan.c@orioninc.com>
2025-08-07 11:15:22 +0530

    Update 03-resource-group.tf

--- 24-Azure-AKS-Terraform/24-02-Terraform-Language-Basics/terraform-manifests-aks/03-resource-group.tf
+++ 24-Azure-AKS-Terraform/24-02-Terraform-Language-Basics/terraform-manifests-aks/03-resource-group.tf
@@ -1,6 +1,10 @@
 # Terraform Resource to Create Azure Resource Group with Input Variables defined in variables.tf
 
 resource "azurerm_resource_group" "aks_rg" {
-  name = "terraform-aks"
-  location = "eastus"
+  name = var.resource_group_name
+  location = var.location
+  tags = {
+    environment = var.environment
+    created_by  = "Terraform"
+  }
 }

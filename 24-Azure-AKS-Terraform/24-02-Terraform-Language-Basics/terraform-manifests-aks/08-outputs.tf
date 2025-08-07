7e31e01076d561f493d732afd2e1301d67039e88
Pawan Chaudhary <Pawan.C@Orioninc.com>
2025-08-07 15:48:58 +0530

    Updating output file.

--- 24-Azure-AKS-Terraform/24-02-Terraform-Language-Basics/terraform-manifests-aks/08-outputs.tf
+++ 24-Azure-AKS-Terraform/24-02-Terraform-Language-Basics/terraform-manifests-aks/08-outputs.tf
@@ -15,3 +15,22 @@ output "resource_group_name" {
   value = azurerm_resource_group.aks_rg.name
   
 }
+
+variable "ssh_public_key" {
+    default = "~/.ssh/aks-prod-sshkeys-terraform/aksprodsshkey.pub"
+    description = "This variable defines the SSH Public Key for Linux k8s Worker nodes"  
+}
+
+# Windows Admin Username for k8s worker nodes
+variable "windows_admin_username" {
+    type = string
+    default = "azureuser"
+    description = "This variable defines the Windows admin username k8s Worker nodes"  
+}
+
+# Windows Admin Password for k8s worker nodes
+variable "windows_admin_password" {
+    type = string
+    default = "HiIAmFromGorakhpur@1984"
+    description = "This variable defines the Windows admin password k8s Worker nodes"  
+}				

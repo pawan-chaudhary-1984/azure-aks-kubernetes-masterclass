04921132cc4e2552c090f5f2ac1e50f334cf2412
pawan-chaudhary-1984 <pawan.c@orioninc.com>
2025-08-07 10:29:24 +0530

    Update 08-outputs.tf

--- 24-Azure-AKS-Terraform/24-02-Terraform-Language-Basics/terraform-manifests-aks/08-outputs.tf
+++ 24-Azure-AKS-Terraform/24-02-Terraform-Language-Basics/terraform-manifests-aks/08-outputs.tf
@@ -2,15 +2,3 @@
 # 1. Resource Group Location
 # 2. Resource Group Id
 # 3. Resource Group Name
-
-output "location" {
-  value = azurerm_resource_group.aks_rg.location
-}
-
-output "resource_group_id" {
-  value = azurerm_resource_group.aks_rg.id
-}
-
-output "resource_group_name" {
-  value = azurerm_resource_group.aks_rg.name
-}

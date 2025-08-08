971c8ede97a25d866fa7c53da37a24f6dad060db
pawan-chaudhary-1984 <pawan.c@orioninc.com>
2025-08-08 23:05:04 +0530

    Update 09-aks-cluster-linux-user-nodepools.tf

--- 24-Azure-AKS-Terraform/24-04-Create-AKS-NodePools-using-Terraform/terraform-manifests-aks/09-aks-cluster-linux-user-nodepools.tf
+++ 24-Azure-AKS-Terraform/24-04-Create-AKS-NodePools-using-Terraform/terraform-manifests-aks/09-aks-cluster-linux-user-nodepools.tf
@@ -2,7 +2,7 @@
 resource "azurerm_kubernetes_cluster_node_pool" "linux101" {
   #availability_zones    = [1, 2, 3]
   # Added June 2023
-  zones = [ 1, 2, 3 ]
+  # zones = [ 1, 2, 3 ]
   #enable_auto_scaling  = true # COMMENTED OCT2024
   auto_scaling_enabled = true  # ADDED OCT2024
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id

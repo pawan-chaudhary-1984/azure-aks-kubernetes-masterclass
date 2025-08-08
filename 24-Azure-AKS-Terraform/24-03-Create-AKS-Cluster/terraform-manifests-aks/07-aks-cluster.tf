3f50e54bc26ad6c045aa70cfbd77453a652e66c2
Pawan Chaudhary <Pawan.C@Orioninc.com>
2025-08-08 16:24:00 +0530

    Updating

--- 24-Azure-AKS-Terraform/24-03-Create-AKS-Cluster/terraform-manifests-aks/07-aks-cluster.tf
+++ 24-Azure-AKS-Terraform/24-03-Create-AKS-Cluster/terraform-manifests-aks/07-aks-cluster.tf
@@ -37,7 +37,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     # node_count = 1
     vm_size    = "Standard_DS2_v2"
     orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
-    zones = [1, 2, 3]
+  
     # enable_auto_scaling = true
     auto_scaling_enabled = true
     min_count = 1
